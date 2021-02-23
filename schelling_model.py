1bd7a4f38918bd443f550d6e83ac3abdcd54ae29
Ashay Aswale <asaswale@wpi.edu>
2021-02-22 21:59:42 -0500

    Working with ratio of t

--- schelling_model.py
+++ schelling_model.py
@@ -2,28 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import random
+import sys
 
-
-p = 0.8
-t1 = 3
-t2 = 4
-x_val = 2
-o_val = 4
+p = 0.6         # Fraction of cells filled
+t1 = 3          # Satisfaction threshold
+t2 = 5          # Will only be used if t1_ratio is not 1. Second satisfaction Threshold
 t1_ratio = 1.0  # When you want 0.8/0.2 ratio of t1 and t2, set this as 0.8
+rounds = 100
 
+x_val = [50,49]
+o_val = [100,99]
 
 def generateBoard(x, y):
     board = np.zeros([x,y])
     total_cells = x*y
     filled_cells = int(total_cells*p)
     x_o_pos = random.sample(range(1, total_cells), filled_cells)
+    
     flag = True
     for cell in x_o_pos:
         row, col = divmod(cell, x)
         if flag:
-            board[row, col] = x_val
+            board[row, col] = random.choices(x_val, weights=(t1_ratio, 1-t1_ratio), k=1)[0]
         else:
-            board[row, col] = o_val
+            board[row, col] = random.choices(o_val, weights=(t1_ratio, 1-t1_ratio), k=1)[0]
         flag = not flag
     return board
 
@@ -44,30 +46,38 @@ def getUnsatisfiedCells(board, row, unsatis, empty_cell):
         main_val = board[row,col]
 
         if main_val == 0:
-            x_nbrs = np.count_nonzero(neighbours == x_val)
-            o_nbrs = np.count_nonzero(neighbours == o_val)
+            x_nbrs = np.count_nonzero(neighbours == x_val[0])
+            x_nbrs += np.count_nonzero(neighbours == x_val[1])
+            o_nbrs = np.count_nonzero(neighbours == o_val[0])
+            o_nbrs += np.count_nonzero(neighbours == o_val[1])
 
             empty_cell = np.concatenate((empty_cell, [[row, col, x_nbrs, o_nbrs]]))
         else:
+            is_normal = (main_val%10==0)
+            sub_val = (main_val - 1) if (is_normal) else main_val +1
             count = np.count_nonzero(neighbours == main_val) - 1
-            if count<t1:
-                unsatis = np.concatenate((unsatis, [[row, col]]))
-    
+            count += np.count_nonzero(neighbours == sub_val)
+            if (is_normal):
+                if (count<t1):
+                    unsatis = np.concatenate((unsatis, [[row, col]]))
+            else:
+                if (count<t2):
+                    unsatis = np.concatenate((unsatis, [[row, col]]))
     return unsatis, empty_cell
 
 
 def getNearestEmptyIndex(board, empty_cell, agent, main_val):
-    col = 2 if main_val == x_val else 3
-    valid_cells = empty_cell[np.where(empty_cell[:,col]>=t1)]
+    col = 2 if (main_val in x_val) else 3
+    is_normal = (main_val%10)==0
+    t = t1 if (is_normal) else t2
+    valid_cells = empty_cell[np.where(empty_cell[:,col]>=t)]
+
     if not (len(valid_cells) > 0):
         return []
     valid_cells = valid_cells[:, 0:2]
     distances = np.sum(valid_cells - agent, axis=1)
-    # print(valid_cells)
     final_cell = valid_cells[np.argmin(distances)]
-    # print(final_cell)
-    # print(empty_cell[:,0:2])
-    # return 
+    
     return np.where(np.all(empty_cell[:,0:2] == final_cell, axis=1))
 
 
@@ -115,8 +125,10 @@ def updateEmptyCell(board, empty_cell):
 
         neighbours = board[l_row:u_row, l_col:u_col].flatten()
 
-        x_nbrs = np.count_nonzero(neighbours == x_val)
-        o_nbrs = np.count_nonzero(neighbours == o_val)
+        x_nbrs = np.count_nonzero(neighbours == x_val[0])
+        x_nbrs += np.count_nonzero(neighbours == x_val[1])
+        o_nbrs = np.count_nonzero(neighbours == o_val[0])
+        o_nbrs += np.count_nonzero(neighbours == o_val[1])
 
         empty_cell[i] = [row, col, x_nbrs, o_nbrs]
         
@@ -128,23 +140,40 @@ def manageBoard(board):
     for i in range(len(unsatis)):
         agent = unsatis[i]
         main_val = board[agent[0], agent[1]]
-        strike = x_strike if main_val == x_val else o_strike
+        strike = x_strike if (main_val in x_val) else o_strike
         val, swapped = swapToNearest(board, empty_cell, agent, main_val)
         updateEmptyCell(board, empty_cell)
+    if len(unsatis) == 0:
+        return True
+    return False
 
 
 if __name__ == '__main__':
 
     board = generateBoard(50,50)
     # print(board, "\n\n\n")
-
+    print("Visualizing the board\nPlease close the window to start the code\n\n")
     visualizeBoard(board)
-    rounds = 100
+
+    print("Completion progress bar")
+    toolbar_width = 100
+    # setup toolbar
+    sys.stdout.write("[%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    perct_value = 0
+
     for i in range(rounds):
-        # if i % (rounds/10) == 0:
-        #     print(i)
-        manageBoard(board)
+        if(manageBoard(board)):
+            print("\nSolved in ", i, " steps")
+            break
+        perct_comp = i/rounds*100
+        if perct_value < perct_comp:
+            sys.stdout.write("#")
+            sys.stdout.flush()
+            perct_value += 1
 
     # print("############")
     # print(board)
+    sys.stdout.write("]\n")
     visualizeBoard(board)
